5c9c5f5d1ad893c7655fcc02b0eaf912bb9fcbfd
Feng Yan <2316696377@qq.com>
2023-04-15 17:27:36 +0800

    a

---
+++ file_testing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# -b filename : Block special file
+# -c filename : Special character file
+# -d directory name : Check for directory existence
+# -e filename : Check for file existence
+# -f filename : Check for regular file existence not a directory
+# -G filename : Check if file exists and owned by effective group ID
+# -g filename : true if file exists and is set-group-id
+# -k filename : Sticky bit
+# -L filename : Symbolic link
